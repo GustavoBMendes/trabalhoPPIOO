d638f1add7a89629eccd8fda23668868d781d9b5
GustavoBMendes <mendes.gustavob@gmail.com>
2019-05-12 20:11:08 -0300

    Funcionando, falta imprimir o passo a passo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-extern crate queues;
 use std::io;
-use std::collections::LinkedList;
-use queues::*;
 
 fn cabecalho() {
     println!("");
@@ -25,6 +22,7 @@ fn main() {
     io::stdin().read_line(&mut expr)
     	.expect("Falha na leitura da expressão!");
 
+	println!("");
     println!("A expressão lida foi: {}", expr);
     
     println!("Dividindo a expressão em tokens e inserindo em uma lista.");
@@ -32,26 +30,26 @@ fn main() {
     let mut i = 0;
     let mut j = 0;                          		//índices i e j para pegar determinado token
 
-	let mut tokensList: Vec<&str> = Vec::new();		//lista de tokens
+	let mut tokens_list: Vec<&str> = Vec::new();		//lista de tokens
     for c in expr.chars() {
     	
     	if c == ' ' || c == '\n'{           		//encontrou espaço ou fim de linha, pegar tokens
 			let token = &expr[j..i];
-			tokensList.push(token);
+			tokens_list.push(token);
 			j = i+1;
     	}
 
 		else if c == '('{                   //encontrou parenteses
 			i += 1;
 			let parenteses = &expr[j..i];
-			tokensList.push(parenteses);
+			tokens_list.push(parenteses);
     		j = i;
 			continue;
 		}
 
 		else if c == ')' {                  //fecha parenteses
 			let parenteses = &expr[j..i];
-			tokensList.push(parenteses);
+			tokens_list.push(parenteses);
     		j = i;
 			i += 1;
 			continue;
@@ -61,75 +59,98 @@ fn main() {
     }
 
     println!("Imprimindo tokens da lista...");
-	println!("{:?}", tokensList);
+	println!("{:?}", tokens_list);
+
+
+// *********************** ************************************** ***********************
+// *********************** PILHA DE OPERADORES E FILA DE EXECUÇÃO ***********************
+// *********************** ************************************** ***********************
+
 
 	println!("Criando a pilha de operadores e a fila de saída dos tokens...");
 	let mut stack: Vec<&str> = Vec::new();
 	let mut fila: Vec<&str> = Vec::new();
 	
-	let mut k = 0;
+	//let mut k: isize = -1;
 
-	for tokens in tokensList.iter() {
+	for tokens in tokens_list.iter() {
 
 		if tokens != &"*" && tokens != &"/" && tokens != &"+" && tokens != &"-" && tokens != &"(" && tokens != &")" {
 			fila.push(tokens); 			//é um número, adicionar na fila de saída
 		}
 
-		else if tokens == &"*" || tokens == &"(" {
+		else if tokens == &"(" {
 			stack.push(tokens);	//operadores de maior precedencia, empilha
-			k += 1;
 		}
 
-		else if tokens == &"/" {
-			while stack[k-1] == "*" {
-				let mut op = stack[k-1];
+		else if tokens == &"*" {
+			while stack.last() == Some(&"*") || stack.last() == Some(&"/") {
+				let op = stack[stack.len()-1];
 				fila.push(op);
-				k -= 1;
 				stack.pop();
 			}
 			stack.push(tokens);
-			k += 1;
 		}
 
-		else if tokens == &"+" || tokens == &"-" {
-			if stack.is_empty() || (stack[k-1] != "*" && stack[k-1] != "/") {
+		else if tokens == &"/" {
+			while stack.last() == Some(&"*") || stack.last() == Some(&"/") {
+				let op = stack[stack.len()-1];
+				fila.push(op);
+				stack.pop();
+			}
+			stack.push(tokens);
+		}
+
+		else if tokens == &"+" {
+			if stack.is_empty() || (stack.last() != Some(&"*") && stack.last() != Some(&"/") && stack.last() != Some(&"-") && stack.last() != Some(&"+")) {
 				stack.push(tokens);	//operadores de menor precedencia
-				k += 1;				//empilha caso nao existe um de maior precedencia no topo
+									//empilha caso nao existe um de maior precedencia no topo
+			}
+
+			else {
+				while (stack.last() ==  Some(&"*") || stack.last() == Some(&"/") || stack.last() == Some(&"-") || stack.last() == Some(&"+")) && stack.len() >= 1 {
+					let op = stack[stack.len()-1];
+					fila.push(op);			//fila recebe operador
+					stack.pop();
+				}
+				stack.push(tokens);
+			}
+		}
+
+		else if tokens == &"-" {
+			if stack.is_empty() || (stack.last() != Some(&"*") && stack.last() != Some(&"/") && stack.last() != Some(&"+") && stack.last() != Some(&"-")) {
+				stack.push(tokens);	//operadores de menor precedencia
+									//empilha caso nao existe um de maior precedencia no topo
 			}
 
 			else {
 				
-				while stack[k-1] ==  "*" || stack[k-1] == "/" {
-					let mut op = stack[k-1];
+				while stack.last() ==  Some(&"*") || stack.last() == Some(&"/") || stack.last() == Some(&"+") || stack.last() == Some(&"-") {
+					let op = stack[stack.len()-1];
 					fila.push(op);			//fila recebe operador
-					k -= 1;
 					stack.pop();
 				}
 				stack.push(tokens);
-				k += 1;
 			}
 		}
 
 		else if tokens == &")" {
-			while stack[k-1] != "(" {
-				let mut op = stack[k-1];
+			while stack.last() != Some(&"(") {
+				let op = stack[stack.len()-1];
 				fila.push(op);			//fila recebe op
-				k -= 1;
 				stack.pop();
 			}
 			stack.pop();
-			k -= 1;
 		}	
 
 	}
 	//acabou lista de tokens, desempilha todo o resto da pilha e insere na fila
 	
-	println!("{}", k);
+	println!("{}", stack.len());
 
-	while k >= 1 {
-		let mut op = stack[k-1];
+	while stack.len() >= 1 {
+		let op = stack[stack.len()-1];
 		fila.push(op);						//insere op na fila
-		k -= 1;
 		stack.pop();
 	}
 
@@ -138,21 +159,21 @@ fn main() {
 	}
 	println!("");
 
-	println!("Criando pilha de execução dos operadores");
-	let mut pilhaOperacoes: Vec<i32> = Vec::new();
+	println!("Criando pilha de execução dos operandos");
+	let mut pilha_operacoes: Vec<i64> = Vec::new();
 	let mut x = 0;
-	let mut num1 = 0;
-	let mut num2 = 0;
-	//let mut resultParcSS: &str;
+	let mut num1: i64;
+	let mut num2: i64;
+	//let mut result_parcSS: &str;
 
 	//para cada numero encontrado na fila da notação reversa, ele sera inserido na pilha para realizar uma operação
 	for token in fila.iter(){	
 		
 		//se for um número, adicionar na pilha de operações 
 		if token != &"*" && token != &"/" && token != &"+" && token != &"-" {
-			pilhaOperacoes.push(token.parse::<i32>().unwrap());
+			pilha_operacoes.push(token.parse::<i64>().unwrap());
 			x += 1;
-			println!("Pilha operações[{}] = {}", x, pilhaOperacoes[x-1]);
+			println!("Pilha operações[{}] = {}", x, pilha_operacoes[x-1]);
 		}
 
 
@@ -160,42 +181,40 @@ fn main() {
 		//realiza a operação, empilha o resultado
 		//mostra a equação com o resultado parcial
 
-		else if pilhaOperacoes.len() >= 2 {
+		else if pilha_operacoes.len() >= 2 {
 
-			num2 = pilhaOperacoes[x-1];
-			pilhaOperacoes.pop(); 
+			num2 = pilha_operacoes[x-1];
+			pilha_operacoes.pop(); 
 			x -= 1;
-			num1 = pilhaOperacoes[x-1];
-			pilhaOperacoes.pop();
+			num1 = pilha_operacoes[x-1];
+			pilha_operacoes.pop();
 
 			if token == &"+" {
-				let resultParc = num1 + num2;
-				pilhaOperacoes.push(resultParc);
+				let result_parc = num1 + num2;
+				pilha_operacoes.push(result_parc);
 			}
 
 			else if token == &"-" {
-				let resultParc = num1 - num2;
-				pilhaOperacoes.push(resultParc);
+				let result_parc = num1 - num2;
+				pilha_operacoes.push(result_parc);
 			}
 
 			else if token == &"*" {
-				let resultParc = num1 * num2;
-				pilhaOperacoes.push(resultParc);
+				let result_parc = num1 * num2;
+				pilha_operacoes.push(result_parc);
 			}
 
 			else if token == &"/" {
-				let resultParc = num1 / num2;
-				pilhaOperacoes.push(resultParc);
+				let result_parc = num1 / num2;
+				pilha_operacoes.push(result_parc);
 			}
 
-			//let resultParc = resultParc.to_string();
-			//resultParcSS = &resultParc.to_string();
-			println!("Pilha operações[{}] = {}", x, pilhaOperacoes[x-1]);
+			println!("Pilha operações[{}] = {}", x, pilha_operacoes[x-1]);
 		}
 
 	}
 
-	for results in pilhaOperacoes.iter() {
+	for results in pilha_operacoes.iter() {
 		println!("Resultado: {}", results);
 	}
 
